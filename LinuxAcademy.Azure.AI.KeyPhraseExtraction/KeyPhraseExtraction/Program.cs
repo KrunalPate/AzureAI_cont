7b1db9cbd61ca0dbc8aaac752458b4c78d9ca642
Krunal Patel <patel.krunal.tech@gmail.com>
2024-06-20 13:12:47 +0000

    Initial Commit

---
+++ LinuxAcademy.Azure.AI.KeyPhraseExtraction/KeyPhraseExtraction/Program.cs
@@ -0,0 +1,35 @@
+﻿using KeyPhraseExtraction.support;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyPhraseExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endpoint = ; // insert your endpoint
+            string key = ; // insert your key
+
+            var credentials = new ApiKeyServiceClientCredentials(key);
+            var client = new TextAnalyticsClient(credentials)
+            {
+                Endpoint = endpoint
+            };
+
+            var result = client.KeyPhrases("My cat might need to see a veterinarian.");
+
+            // Printing key phrases
+            Console.WriteLine("Key phrases:");
+
+            foreach (string keyphrase in result.KeyPhrases)
+            {
+                Console.WriteLine($"\t{keyphrase}");
+            }
+        }
+    }
+}
